d19d4cb78ccdd10b91633bae508f471bb65baf2e
lanboys <lan_bing2013@163.com>
2022-09-11 16:54:40 +0800

    layout测试

--- logback-examples/src/main/java/chapters/layouts/SampleLogging.java
+++ logback-examples/src/main/java/chapters/layouts/SampleLogging.java
@@ -1,13 +1,13 @@
 /**
  * Logback: the reliable, generic, fast and flexible logging framework.
  * Copyright (C) 1999-2015, QOS.ch. All rights reserved.
- *
+ * <p>
  * This program and the accompanying materials are dual-licensed under
  * either the terms of the Eclipse Public License v1.0 as published by
  * the Eclipse Foundation
- *
- *   or (per the licensee's choosing)
- *
+ * <p>
+ * or (per the licensee's choosing)
+ * <p>
  * under the terms of the GNU Lesser General Public License version 2.1
  * as published by the Free Software Foundation.
  */
@@ -15,6 +15,8 @@ package chapters.layouts;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.net.URL;
 
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
@@ -24,7 +26,11 @@ import ch.qos.logback.core.util.StatusPrinter;
 public class SampleLogging {
 
     public static void main(String[] args) {
+        sampleLog("sampleLayoutConfig.xml");
+        // sampleLog("sampleLayoutConfig2.xml");
+    }
 
+    private static void sampleLog(String filename) {
         Logger logger = LoggerFactory.getLogger(SampleLogging.class);
         LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
 
@@ -32,7 +38,7 @@ public class SampleLogging {
             JoranConfigurator configurator = new JoranConfigurator();
             lc.reset();
             configurator.setContext(lc);
-            configurator.doConfigure(args[0]);
+            configurator.doConfigure(SampleLogging.class.getResource("/chapters/layouts/" + filename).getPath());
         } catch (JoranException je) {
             // StatusPrinter will handle this
         }
